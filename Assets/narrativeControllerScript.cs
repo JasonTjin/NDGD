c5f8adff4c3babfedf156e43afe6b181348adab0
spencermcnamara <smcn0012@student.monash.edu>
2024-07-31 10:18:03 +1000

    Gave narrative controller access to change the text

--- Assets/narrativeControllerScript.cs
+++ Assets/narrativeControllerScript.cs
@@ -1,33 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NarrativeControllerScript : MonoBehaviour
 {
-    public int storyStatus;
-    public int choice;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string leftArrowText = "";
+    public string rightArrowText = "";
+    public string upArrowText = "";
+    public TMP_Text leftArrowTextComponent;
+    public TMP_Text rightArrowTextComponent;
+    public TMP_Text upArrowTextComponent;
 
     public void LeftArrowSelected(){
         Debug.Log("Left Arrow Selected");
+        updateArrows();
     }
 
     public void RightArrowSelected(){
         Debug.Log("Right Arrow Selected");
+        updateArrows();
     }
 
     public void UpArrowSelected(){
         Debug.Log("Up Arrow Selected");
+        updateArrows();
+    }
+
+    private void updateArrows(){
+        leftArrowTextComponent.text = leftArrowText;
+        rightArrowTextComponent.text = rightArrowText;
+        upArrowTextComponent.text = upArrowText;
     }
 }
