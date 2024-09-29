ad6e7a73714aaeb52e8b4ef1bb1f026fe9b71984
JasonTjin <jtji0003@student.monash.edu>
2024-09-29 18:19:20 +1000

    Summary screen score colour coded and dialogue 8 animation changes

--- Assets/Scripts/SummaryScript.cs
+++ Assets/Scripts/SummaryScript.cs
@@ -12,6 +12,9 @@ public class summaryScript : MonoBehaviour
     private string financeScoreText;
     private string teamMoraleText;
     private string moralityScoreText;
+    private int financeScoreInt;
+    private int teamMoraleInt;
+    private int moralityScoreInt;
 
     // Update is called once per frame
     public void UpdateScores(int newFinanceScore, int newTeamMorale, int newMoralityScore)
@@ -19,22 +22,87 @@ public class summaryScript : MonoBehaviour
         financeScoreText = (newFinanceScore.ToString() + '%');
         teamMoraleText = (newTeamMorale.ToString() + '%');
         moralityScoreText = (newMoralityScore.ToString() + '%');
+
+        financeScoreInt = newFinanceScore;
+        teamMoraleInt = newTeamMorale;
+        moralityScoreInt = newMoralityScore;
     }
 
-    void Start(){
+    void Start()
+    {
         financeScore = GameObject.FindGameObjectWithTag("FinanceScore").GetComponent<TMP_Text>();
         teamMorale = GameObject.FindGameObjectWithTag("TeamMorale").GetComponent<TMP_Text>();
         moralityScore = GameObject.FindGameObjectWithTag("MoralityScore").GetComponent<TMP_Text>();
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<gameManagerScript>();
     }
 
-    void Update(){
+    void Update()
+    {
         financeScore.text = financeScoreText;
         teamMorale.text = teamMoraleText;
         moralityScore.text = moralityScoreText;
+
+        Color darkGreen, green, yellow, red;
+        ColorUtility.TryParseHtmlString("#006124", out darkGreen);  
+        ColorUtility.TryParseHtmlString("#03E054", out green); 
+        ColorUtility.TryParseHtmlString("#DED700", out yellow);   
+        ColorUtility.TryParseHtmlString("#AE0800", out red);   
+
+        // Change colour of score based on the score
+        if (financeScoreInt >= 90)
+        {
+            financeScore.color = darkGreen;
+        }
+        else if (financeScoreInt >= 70)
+        {
+            financeScore.color = green;
+        }
+        else if (financeScoreInt >= 50)
+        {
+            financeScore.color = yellow;
+        }
+        else
+        {
+            financeScore.color = red;
+        }
+        // Update Morale Score Text Color
+        if (teamMoraleInt >= 90)
+        {
+            teamMorale.color = darkGreen;
+        }
+        else if (teamMoraleInt >= 70)
+        {
+            teamMorale.color = green;
+        }
+        else if (teamMoraleInt >= 50)
+        {
+            teamMorale.color = yellow;
+        }
+        else
+        {
+            teamMorale.color = red;
+        }
+        // Update Morality Score Text Color
+        if (moralityScoreInt >= 90)
+        {
+            moralityScore.color = darkGreen;
+        }
+        else if (moralityScoreInt >= 70)
+        {
+            moralityScore.color = green;
+        }
+        else if (moralityScoreInt >= 50)
+        {
+            moralityScore.color = yellow;
+        }
+        else
+        {
+            moralityScore.color = red;
+        }
     }
 
-    public void Continue(){
+    public void Continue()
+    {
         gameManager.GoToDialogue();
     }
 }
